e04ca17cd2e62b2b7980f417c4d70b4c07ba7890
agrube <agrube@a18fe7de-8022-b628-27ae-5a07db8d0f9a>
2011-08-14 01:18:27 +0000

    Added tongue activity.

--- ChameleonChase/ChameleonChase/Chameleon.cs
+++ ChameleonChase/ChameleonChase/Chameleon.cs
@@ -18,15 +18,18 @@ namespace ChameleonChase
     public class Chameleon : PositionedObject
     {
         #region Constants
-        private const float DEFAULT_CHAMEL_VELOCITY = 51.0f;
-        private const float SLOW_CHAMEL_VELOCITY = 45.0f;
+        private const float DEFAULT_CHAMEL_VELOCITY = 25.0f;
         private const float DEFAULT_CHAMEL_POS = -10.0f;
         
-        private const float LASER_VELOCITY = 10.0f;
+        private const float LASER_VELOCITY = 20.0f;
         private const float LASER_TRIGGER_DISTANCE = 100.0f;
-        private const int LASER_ROW_LENGTH = 90;
 
-        private const float TONGUE_OFFSET = 200.0f;
+        private const float TONGUE_VELOCITY = 15.0f;
+        private const float TONGUE_OFFSET = 50.0f;
+        private const float TONGUE_TRIGGER_DISTANCE = 100.0f;
+        private const float TONGUE_START = 5.0f;
+
+        private const float RAIL_DISTANCE = 10.0f;
 
         #endregion
 
@@ -35,14 +38,19 @@ namespace ChameleonChase
         // Here you'd define things that your Entity contains, like Sprites
         // or Circles:
         private Sprite mVisibleRepresentation;
-        private Circle mCollision;
+        //private Circle mCollision;
+        private Polygon mCollision;
 
         private Circle mLaser;
-        private Polygon mTongue;
+        private AxisAlignedRectangle mTongue;
 
         private float mLastLaserPos;
         private bool mIsLasering;
-        private int mLaserDistance;
+        private int mLaserRail;
+
+        private float mLastTonguePos;
+        private bool mIsLicking;
+        private int mTongueRail;
 
         // Keep the ContentManager for easy access:
         string mContentManagerName;
@@ -97,19 +105,40 @@ namespace ChameleonChase
             mVisibleRepresentation = SpriteManager.AddSprite("redball.bmp", mContentManagerName);
             mVisibleRepresentation.AttachTo(this, false);
 
-            mCollision = ShapeManager.AddCircle();
+            //mCollision = ShapeManager.AddCircle();
+            //mCollision.AttachTo(this, false);
+
+            Point[] pointArray = 
+            {
+               new Point(-20,  10), // top left
+               new Point(  5,  10), // top right
+               new Point( -5, -10), // bottom right
+               new Point(-20, -10), // bottom left
+               new Point(-20,  10)  // repeat top left to close Polygon
+            };
+
+            mCollision = ShapeManager.AddPolygon();
             mCollision.AttachTo(this, false);
+            mCollision.Points = pointArray;
 
             mLaser = ShapeManager.AddCircle();
             mLaser.AttachTo(this, false);
             mLaser.Visible = false;
 
+            mTongue = ShapeManager.AddAxisAlignedRectangle();
+            mTongue.ScaleX = 0.5f;
+            mTongue.AttachTo(this, false);
+            mTongue.Visible = false;
+
             this.X = DEFAULT_CHAMEL_POS;
             this.XVelocity = DEFAULT_CHAMEL_VELOCITY;
 
             mLaser.RelativeX = DEFAULT_CHAMEL_POS;
-            //mLaser.RelativeXVelocity = LASER_VELOCITY;
             this.mLastLaserPos = DEFAULT_CHAMEL_POS;
+            mLaserRail = 3;
+
+            mTongue.RelativeX = TONGUE_START;
+            this.mLastTonguePos = TONGUE_OFFSET;
         }
 
 
@@ -117,15 +146,24 @@ namespace ChameleonChase
         {
             // This code should do things like set Animations, respond to input, and so on.
             bool activateLaser = (this.X - mLastLaserPos > LASER_TRIGGER_DISTANCE);
+            bool activateTongue = (this.X - mLastTonguePos > TONGUE_TRIGGER_DISTANCE);
 
             //Console.Out.WriteLine("Activate Laser: " + activateLaser);
 
             if (mIsLasering)
                 activateLaser = true;
 
+            if (mIsLicking)
+                activateTongue = true;
+
             if (activateLaser)
             {
                 LaserAttack();
+            }
+
+            if (activateTongue)
+            {
+                TongueAttack();
             }
 
         }
@@ -146,44 +184,112 @@ namespace ChameleonChase
         {
             if (!mIsLasering)
             {
-                mLaser.RelativeY = 5.0f;
+                mLaser.RelativeY = RAIL_DISTANCE;
                 mLaser.RelativeX = 0.0f;
                 mLaser.Visible = true;
 
                 mLaser.RelativeXVelocity = LASER_VELOCITY;
                 mIsLasering = true;
-                mLaserDistance = 0;
+                mLaserRail = 3;
             }
             else
             {
-                if (mLaserDistance == LASER_ROW_LENGTH)
+                if ((mLaserRail == 3) && (mLaser.X >= SpriteManager.Camera.AbsoluteRightXEdgeAt(0)))
                 {
+                    mLaserRail = 2;
                     mLaser.RelativeY = 0.0f;
                     mLaser.RelativeXVelocity = -(LASER_VELOCITY);
                 }
-                else if (mLaserDistance == (LASER_ROW_LENGTH * 2))
+                else if ((mLaserRail == 2) && (mLaser.X <= this.X - 5.0f))
                 {
-                    mLaser.RelativeY = -5.0f;
+                    mLaserRail = 1;
+                    mLaser.RelativeY = -RAIL_DISTANCE;
                     mLaser.RelativeXVelocity = LASER_VELOCITY;
+                    //mLaser.RelativeX = this.X - 7.0f;
                 }
-                else if (mLaserDistance == (LASER_ROW_LENGTH * 3))
+                else if ((mLaserRail == 1) && (mLaser.X >= SpriteManager.Camera.AbsoluteRightXEdgeAt(0)))
                 {
+                    mLaserRail = 0;
                     mLaser.RelativeXVelocity = 0;
                     mLaser.Visible = false;
                     mIsLasering = false;
                     mLastLaserPos = this.X;
                 }
 
-                mLaserDistance++;
+                #region Old Laser Code
+                //if (mLaserDistance == LASER_ROW_LENGTH)
+                //{
+                //    mLaser.RelativeY = 0.0f;
+                //    mLaser.RelativeXVelocity = -(LASER_VELOCITY);
+                //}
+                //else if (mLaserDistance == (LASER_ROW_LENGTH * 2))
+                //{
+                //    mLaser.RelativeY = -5.0f;
+                //    mLaser.RelativeXVelocity = LASER_VELOCITY;
+                //}
+                //else if (mLaserDistance == (LASER_ROW_LENGTH * 3))
+                //{
+                //    mLaser.RelativeXVelocity = 0;
+                //    mLaser.Visible = false;
+                //    mIsLasering = false;
+                //    mLastLaserPos = this.X;
+                //}
+
+                //mLaserDistance++;
+
+                #endregion
             }
         }
 
         private void TongueAttack()
         {
+            if (!mIsLicking)
+            {
+                mTongue.Visible = true;
 
+                mTongue.RelativeXVelocity = TONGUE_VELOCITY;
+                mIsLicking = true;
+                mTongueRail = CreateRandomNumber();
+
+                if (mTongueRail == 3)
+                {
+                    mTongue.RelativeX = TONGUE_START;
+                    mTongue.RelativeY = RAIL_DISTANCE;
+                }
+                else if (mTongueRail == 2)
+                {
+                    mTongue.RelativeX = TONGUE_START - 5.0f;
+                    mTongue.RelativeY = 0.0f;
+                }
+                else if (mTongueRail == 1)
+                {
+                    mTongue.RelativeX = TONGUE_START - 10.0f;
+                    mTongue.RelativeY = -(RAIL_DISTANCE);
+                }
+            }
+            else
+            {
+                mTongue.ScaleX += 0.25f;
+                if (mTongue.X >= SpriteManager.Camera.AbsoluteRightXEdgeAt(0) + 5.0f)
+                {
+                    mTongue.RelativeXVelocity = 0.0f;
+                    mTongue.ScaleX = 0.5f;
+                    mTongue.Visible = false;
+
+                    mIsLicking = false;
+                    mLastTonguePos = this.X;
+                }
+            }
         }
 
         #endregion
+
+        private int CreateRandomNumber()
+        {
+            Random r = new Random();
+
+            return r.Next(1, 4);
+        }
         
         #endregion
     }
