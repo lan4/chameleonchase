0548a9dff51d74c99d19485fb890cd255baa9def
lan4@umbc.edu <lan4@umbc.edu@a18fe7de-8022-b628-27ae-5a07db8d0f9a>
2011-08-14 00:45:22 +0000

    added obstacles

---
+++ ChameleonChase/ChameleonChase/Obstacle.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FlatRedBall;
+using FlatRedBall.Graphics;
+using FlatRedBall.Math.Geometry;
+
+
+// Be sure to replace:
+// 1.  The namespace
+// 2.  The class name
+// 3.  The constructor (should be the same as the class name)
+
+
+namespace ChameleonChase
+{
+    public class Obstacle : PositionedObject
+    {
+        #region Fields
+
+        // Here you'd define things that your Entity contains, like Sprites
+        // or Circles:
+        private Sprite mVisibleRepresentation;
+        private Circle mCollision;
+
+        // Keep the ContentManager for easy access:
+        string mContentManagerName;
+
+        private int railPos;
+
+        #endregion
+
+        #region Properties
+
+
+        // Here you'd define properties for things
+        // you want to give other Entities and game code
+        // access to, like your Collision property:
+        public Circle Collision
+        {
+            get { return mCollision; }
+        }
+
+        public int RailPosition
+        {
+            get { return railPos; }
+            set { railPos = value; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        // Constructor
+        public Obstacle(string contentManagerName)
+        {
+            // Set the ContentManagerName and call Initialize:
+            mContentManagerName = contentManagerName;
+
+            // If you don't want to add to managers, make an overriding constructor
+            Initialize(true);
+        }
+
+        protected virtual void Initialize(bool addToManagers)
+        {
+            // Here you can preload any content you will be using
+            // like .scnx files or texture files.
+
+            if (addToManagers)
+            {
+                AddToManagers(null);
+            }
+        }
+
+        public virtual void AddToManagers(Layer layerToAddTo)
+        {
+            // Add the Entity to the SpriteManager
+            // so it gets managed properly (velocity, acceleration, attachments, etc.)
+            SpriteManager.AddPositionedObject(this);
+
+            // Here you may want to add your objects to the engine.  Use layerToAddTo
+            // when adding if your Entity supports layers.  Make sure to attach things
+            // to this if appropriate.
+            mVisibleRepresentation = SpriteManager.AddSprite("Box.png", mContentManagerName);
+
+            float texturePixelWidth = mVisibleRepresentation.Texture.Width;
+            float texturePixelHeight = mVisibleRepresentation.Texture.Height;
+
+            float pixelsPerUnit = SpriteManager.Camera.PixelsPerUnitAt(mVisibleRepresentation.Z);
+
+            mVisibleRepresentation.ScaleX = .25f * texturePixelWidth / pixelsPerUnit;
+            mVisibleRepresentation.ScaleY = .25f * texturePixelHeight / pixelsPerUnit;
+            
+            mVisibleRepresentation.AttachTo(this, false);
+
+            mCollision = ShapeManager.AddCircle();
+            mCollision.AttachTo(this, false);
+
+            railPos = 0;
+        }
+
+
+
+        public virtual void Activity()
+        {
+            // This code should do things like set Animations, respond to input, and so on.
+        }
+
+        public virtual void Destroy()
+        {
+            // Remove self from the SpriteManager:
+            SpriteManager.RemovePositionedObject(this);
+
+            // Remove any other objects you've created:
+            SpriteManager.RemoveSprite(mVisibleRepresentation);
+            ShapeManager.Remove(mCollision);
+        }
+
+        #endregion
+    }
+}
